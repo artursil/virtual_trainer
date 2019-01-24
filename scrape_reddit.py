3198d026ae1a253b213f1ca0d264c04b003eb91e
Ahmad Kurdi <46104490+ahmadofski@users.noreply.github.com>
2019-01-24 16:11:45 +0000

    Update scrape_reddit.py
    Added duration

--- scrape_reddit.py
+++ scrape_reddit.py
@@ -12,11 +12,12 @@ reddit = praw.Reddit(client_id=CLIENT_ID, \
                      password=RDT_PASS)
 rdt_channel = 'formcheck' # subreddit channel
 cutoff = 5 # max number of submissions  to gather
-scrape_dict = { "title":[], 
-                "score":[], 
-                "url":[],  
-                "is_video": [], 
-                "link_flair_text":[]} # body supplied incase a youtube link is there
+scrape_dict = { "title": [], 
+                "score": [], 
+                "url": [],  
+                "is_video": [],
+                "duration": [], 
+                "link_flair_text": [] }
 
 def scrape_rdt():
     # Get Reddit submissions
@@ -26,6 +27,10 @@ def scrape_rdt():
         scrape_dict["score"].append(submission.score)
         scrape_dict["url"].append(submission.url)
         scrape_dict["is_video"].append(submission.is_video)
+        if submission.is_video == True : 
+            scrape_dict["duration"].append(submission.secure_media["reddit_video"]["duration"])
+        else :
+            scrape_dict["duration"].append(0)
         scrape_dict["link_flair_text"].append(submission.link_flair_text)
     return pd.DataFrame(scrape_dict)
 
@@ -33,6 +38,3 @@ def scrape_rdt():
     #filter submissions and download videos
 #    filteredsubs = filter_rdt(rawscrape)
 #    for
-
-#print(scrape_rdt())
-
