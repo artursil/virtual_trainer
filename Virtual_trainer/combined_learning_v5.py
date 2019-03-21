853d5f458f2ac314b7a1f92e19680a3dbda11877
Ahmad <ahmadofski@gmail.com>
2019-03-21 17:12:55 +0100

    fix pandas output

--- Virtual_trainer/combined_learning_v5.py
+++ Virtual_trainer/combined_learning_v5.py
@@ -55,7 +55,7 @@ loss_margin = 0.3
 seed = 1234
 lr, lr_decay = 0.001 , 0.95 
 split_ratio = 0.2
-epochs = 5
+epochs = 12
 batch_size = 512
 n_chunks = 8
 weighting = 0.999 # classification loss weighting
@@ -224,10 +224,10 @@ model = build_model(pretrained, in_joints, in_dims, out_joints, filter_widths, f
 
 epoch=1
 
-#resume_cp = os.path.join(CHECKPATH,'combinedlearning-1st-4.pth')
-#checkp = torch.load(resume_cp)
-#epoch = checkp['epoch']+1
-#model.load_state_dict(checkp['model_state_dict'])
+resume_cp = os.path.join(CHECKPATH,'combinedlearning-v5-533-rank_embed-128-5.pth')
+checkp = torch.load(resume_cp)
+epoch = checkp['epoch']+1
+model.load_state_dict(checkp['model_state_dict'])
 
 #weighting *= weighting_decay**(epoch-1)
 
