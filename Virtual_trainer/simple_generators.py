1a7243424eb75c3cd2d8a0fe93c1dd77aad97e4a
artursil <artur.silicki@gmail.com>
2019-03-22 13:19:00 +0100

    +build_embeds_dataframe

--- Virtual_trainer/simple_generators.py
+++ Virtual_trainer/simple_generators.py
@@ -3,6 +3,8 @@ Simple generator because why needlessly complicate things
 """
 from sklearn.model_selection import train_test_split
 import numpy as np
+import pandas as pd
+import torch
 
 class SimpleSequenceGenerator:
 
@@ -64,19 +66,22 @@ class SimpleSequenceGenerator:
 
 class SimpleSiameseGenerator:
 
-    def __init__(self, batch_len, actions, poses,ratings, pad=0, causal_shift=0, test_split=0.2, random_seed=1234):
+    def __init__(self, batch_len, actions, poses,ratings,model=None,filenames=None, pad=0, causal_shift=0, test_split=0.2, random_seed=1234):
         assert len(actions) == len(poses)
         self.pad = pad
         self.causal_shift = causal_shift
-        self.poses = [np.pad(p,((pad+causal_shift,pad-causal_shift),(0,0),(0,0)),'edge') for p in poses]
+        self.poses_list = [np.pad(p,((pad+causal_shift,pad-causal_shift),(0,0),(0,0)),'edge') for p in poses]
         self.actions = actions
+        self.model=model
+        self.filenames=filenames
+        self.ratings=ratings
         self.batch_len = batch_len
         self.test_split = test_split
         self.random = np.random.RandomState(random_seed)
         self.receptive_field = 2*pad +1
-        self.num_joints = self.poses[0].shape[-2]
-        self.dims = self.poses[0].shape[-1]
-        self.poses = np.stack((np.array(self.poses),np.array(ratings)),axis=1)
+        self.num_joints = self.poses_list[0].shape[-2]
+        self.dims = self.poses_list[0].shape[-1]
+        self.poses = np.stack((np.array(self.poses_list),np.array(ratings)),axis=1)
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.poses, self.actions, stratify=self.actions, test_size=self.test_split)
         self.r_train, self.r_test = self.X_train[:,1] , self.X_test[:,1]  
         self.X_train, self.X_test = self.X_train[:,0] , self.X_test[:,0]  
@@ -160,5 +165,44 @@ class SimpleSiameseGenerator:
                 self.batch_y[i,:] = y_val
                 self.batch_r[i,:] = r_val
             yield  self.batch_X, self.batch_y, self.batch_r
+
+
+    def build_embeds_dataframe(self,df_filename):
+        """
+        Run all videos through the model and save the results.
+        """
+        emb_df = pd.DataFrame(columns=['file','embeddings','rating','exc_class']) 
+        if self.model is None:
+            raise ValueError('You need to specify model to create embeddings')
+        if self.filenames is None:
+            raise ValueError('You need to specify filenames to create embeddings')
+        if self.ratings is None:
+            raise ValueError('You need to specify ratings to create embeddings')
             
-            
+        model = self.model
+        print(len(self.poses_list))
+        for ix,pose in enumerate(self.poses_list):
+            if ix%20==0:
+                print(ix)
+            model.eval()
+    #        if pose.shape[0]<= self.receptive_field:
+    #            continue
+            pose = torch.from_numpy(pose).unsqueeze(0)
+            pose = pose.to(dtype=torch.float)
+            if torch.cuda.is_available():
+                eval_model = model.cuda()
+                pose = pose.cuda()  
+            pred = eval_model(pose).permute(0,2,1)
+            pred = pred.detach().cpu().numpy()
+            pred = pred.squeeze()
+            filename = self.filenames[ix]
+            rating = self.ratings[ix]
+            target = self.actions[ix]
+            for x in range(pred.shape[0]):
+                df_row=dict(file=filename,embeddings = [pred[x]],rating=rating,exc_class=target)
+                
+                emb_df = emb_df.append(pd.DataFrame(df_row),ignore_index=False)
+
+        emb_df.to_csv(f'Data/{df_filename}')
+
+        return self
