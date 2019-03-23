29763dba31ac2a0ff0b383442a0c1fc74913a6b5
Ahmad <ahmadofski@gmail.com>
2019-03-23 14:21:44 +0100

    clipping

--- Virtual_trainer/models/loss.py
+++ Virtual_trainer/models/loss.py
@@ -160,3 +160,115 @@ class CombinedLoss(CustomRankingLoss):
         self.weighting = weighting
     def get_metrics(self):
         return self.metrics
+
+class CustomRankingLoss2(CustomRankingLoss):
+    
+    def forward_(self, embeddings, classes, rankings):
+        # if rankings.dtype==torch.int64:
+        #     rankings = rankings.unsqueeze(0)
+        # if classes.dtype==torch.int64:
+        #     classes = classes.unsqueeze(0)
+        my_zero = torch.zeros(1)
+        my_one = torch.ones(1)
+        my_empty = torch.empty((0))
+        clip_val = torch.tensor(5)
+        if torch.cuda.is_available():
+            my_empty = my_empty.cuda()
+            my_one = my_one.cuda()
+            my_zero = my_zero.cuda()
+            clip_val = clip_val.cuda()
+        pos_distances = my_empty
+        neg_distances = my_empty
+        pairings = []   
+        
+        for ex_class in torch.unique(classes):
+            ex_mask = (classes == ex_class).nonzero()[:,0]
+            pos_mask = (rankings[ex_mask]).nonzero()[:,0]
+            if len(pos_mask)<2:
+                continue
+            neg_mask = (rankings[ex_mask]==0).nonzero()[:,0]
+            pos_dists = torch.max(dist_mat(embeddings[ex_mask[pos_mask]]),dim=1)# find hard positives
+            # save pairings and distances for positives
+            #pos_pairs = np.stack((ex_mask[pos_mask].detach().cpu().numpy().reshape(-1,1),
+            #                      ex_mask[pos_dists[1]].detach().cpu().numpy().reshape(-1,1),
+            #                      pos_dists[0].detach().cpu().numpy().reshape(-1,1), 
+            #                      np.zeros((len(ex_mask[pos_mask]),1)) ), axis=1)
+            
+            pos_pairs = np.stack((rankings[ex_mask[pos_mask]].detach().cpu().numpy().reshape(-1,1),
+                                  rankings[ex_mask[pos_dists[1]]].detach().cpu().numpy().reshape(-1,1),
+                                  pos_dists[0].detach().cpu().numpy().reshape(-1,1), 
+                                  np.tile(ex_class.detach().cpu().numpy(),(len(ex_mask[pos_mask]),1) ) ), axis=1)
+            
+            pairings.append(pos_pairs)
+            pos_distances = torch.cat((pos_distances,pos_dists[0]), dim=0)
+
+            if len(neg_mask)<1:
+                continue
+            for positive in pos_mask:
+                emb_ = embeddings[ex_mask[positive]].repeat(neg_mask.shape[0],1)
+                hard_neg = torch.min(torch.abs(F.pairwise_distance(emb_, embeddings[ex_mask[neg_mask]])),dim=0)
+                # save pairing and distance of hard negative
+                pairings.append(np.array([rankings[ex_mask[positive]].detach().cpu().numpy(),
+                                          rankings[ex_mask[torch.max(hard_neg[1])]].detach().cpu().numpy(),
+                                          torch.max(hard_neg[0]).detach().cpu().numpy(), 
+                                          ex_class.detach().cpu().numpy()]))
+                # recycled rooting flag for clipping
+                clip_neg = torch.min(torch.tensor(torch.max(hard_neg[0])).unsqueeze(0),clip_val) if self.rooting else torch.tensor(torch.max(hard_neg[0])).unsqueeze(0)
+                neg_distances = torch.cat( (neg_distances, clip_neg), dim=0)
+        
+        positive_loss = pos_distances.pow(2)
+        negative_loss = F.relu(self.margin - neg_distances).pow(2)
+        loss = torch.cat((positive_loss, negative_loss)).sqrt().mean()
+
+        self.pairings = pairings
+        
+        return loss
+
+class CombinedLoss2(CustomRankingLoss2):
+
+    def __init__(self, cl_loss, weighting, supress_cl=6, rooting= False, margin=0., size_average=None, reduce=None, reduction='mean'):
+        super().__init__(size_average, reduce, reduction)
+        self.margin = margin
+        self.pairings = []
+        self.weighting = weighting
+        self.cl_loss = cl_loss
+        self.supress_cl = supress_cl
+        self.rooting = rooting
+        self.metrics = None
+
+    def forward(self, embeddings, preds, classes, rankings):
+        weighting = torch.tensor(self.weighting)
+        preds = preds.squeeze()
+        embeddings = embeddings.squeeze()
+        classes = classes.squeeze()
+        # take classification loss
+        classloss = self.cl_loss(preds,classes)
+        
+        # supress control group (non exercise)
+        my_zero = torch.zeros(1)
+        my_one = torch.ones(1)
+        if torch.cuda.is_available():
+            my_one = my_one.cuda()
+            my_zero = my_zero.cuda()
+        mask = torch.nonzero(torch.where(classes == self.supress_cl,my_zero,my_one))[:,0]
+
+        
+        # take ranking loss
+        rankloss = self.forward_(embeddings[mask], classes[mask], rankings[mask])
+        rank_method = "RMSE" if self.rooting else "MSE"
+        print(f"Unweighted losses: Classification C/E {classloss} , Ranking {rank_method} {rankloss}")
+        self.metrics = ( classloss.detach().cpu().numpy() , rankloss.detach().cpu().numpy(), weighting )
+        
+        # scale by weighting
+        if torch.cuda.is_available():
+            weighting = weighting.cuda()
+        loss = torch.mul(rankloss, (1-weighting)) + torch.mul(classloss, weighting) 
+
+        return loss
+    
+    def set_weighting(self, weighting):
+        assert weighting < 1
+        self.weighting = weighting
+    def get_metrics(self):
+        return self.metrics
+
