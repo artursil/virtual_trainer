117a9cc721c79c368712bdffd51e0114f8326d48
artursil <artur.silicki@gmail.com>
2019-03-25 19:16:12 +0100

    +get_keypoints2

--- webcam/webcam_model.py
+++ webcam/webcam_model.py
@@ -1,3 +1,4 @@
+
 import sys
 sys.path.insert(0, "../../virtual_trainer")
 sys.path.insert(0,
@@ -75,6 +76,7 @@ class ModelClass(object):
 
         self.video = cv2.VideoCapture(index)
 
+        self.clip_df_tmp = pd.DataFrame()
         self.clip_df = pd.DataFrame()
 
     def __del__(self):
@@ -164,6 +166,147 @@ class ModelClass(object):
 
                 _, jpeg = cv2.imencode('.jpg', pic_key)
                 return  jpeg.tobytes()
+
+    def get_keypoints2(self):
+        model = self.model
+        with torch.no_grad():
+            model.eval()
+            ix=0
+            frame_counter=0
+            while True:
+                _, image = self.video.read()
+                img, orig_img = VideoScraper.transform_single_frame(image,220)
+
+                X = torch.Tensor(img).unsqueeze(0).to(DEVICE)
+                img = orig_img
+                
+                predicted_outputs, _ = model(X)
+        
+                output1, output2 = predicted_outputs[-2], predicted_outputs[-1]
+                output = output2.detach().cpu().numpy()
+
+                frame = img.copy()
+                frameWidth = frame.shape[1]
+                frameHeight = frame.shape[0]
+                after_shape = img.shape
+                detected_keypoints = []
+                keypoints_list = np.zeros((0,3))
+                keypoint_id = 0
+
+                for part in range(NPOINTS):
+                    probMap = output[0,part,:,:]
+                    probMap = cv2.resize(probMap, (frame.shape[1], frame.shape[0]))
+
+                    keypoints = getKeypoints(probMap, THRESHOLD)
+                    keypoints_with_id = []
+                    for i in range(len(keypoints)):
+                        keypoints_with_id.append(keypoints[i] + (keypoint_id,))
+                        keypoints_list = np.vstack([keypoints_list, keypoints[i]])
+                        keypoint_id += 1
+                
+                    detected_keypoints.append(keypoints_with_id)
+
+                valid_pairs, invalid_pairs = getValidPairs(np.expand_dims(output1[0].detach().cpu().numpy(),axis=0),
+                                                            detected_keypoints,frameWidth, frameHeight)
+                personwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs,keypoints_list)
+
+                spotter=False
+                if len(personwiseKeypoints)>1:
+                    spotter = detect_spotters(personwiseKeypoints)
+
+                if len(personwiseKeypoints)>1:
+                    personwiseKeypoints = more_keypoints(personwiseKeypoints,6)
+
+                if len(personwiseKeypoints)>1:
+                    personwiseKeypoints = bigger_person(personwiseKeypoints,keypoints_list,frameWidth, frameHeight)
+
+                if len(personwiseKeypoints)>1:
+                    personwiseKeypoints = head_middle(personwiseKeypoints,keypoints_list,frameWidth, frameHeight)
+                                
+                if len(personwiseKeypoints)>0:
+                    final_keypoints={}
+                    num_keypoints = 0
+                    for k,x in enumerate(personwiseKeypoints[0][:len(personwiseKeypoints[0])-1]):
+                        final_keypoints[f"{k}_0"] = np.nan if x==-1 else keypoints_list[int(x)][:2][0]
+                        final_keypoints[f"{k}_1"] = np.nan if x==-1 else keypoints_list[int(x)][:2][1]
+                        if x!=-1:
+                            num_keypoints+=1
+                else:
+                    final_keypoints={}       
+                    for k in range(15):
+                        final_keypoints[f"{k}_0"] = np.nan 
+                        final_keypoints[f"{k}_1"] = np.nan 
+                    num_keypoints = 0
+
+                df_dict =  {"vid_nr":ix,"filename":'filename', "clip_id":0, "target":0, 
+                            "num_keypoints":num_keypoints,"spotter":spotter}
+                df_dict.update(final_keypoints)
+                self.clip_df_tmp = self.clip_df_tmp.append(pd.DataFrame(df_dict,index=[0]),ignore_index=True)
+                if frame_counter%30==0:
+                    if len(self.clip_df)<90:
+                        self.clip_df = self.clip_df.append(self.clip_df_tmp)
+                    else:
+                        self.clip_df = self.clip_df.iloc[30:,].append(self.clip_df_tmp)
+                    self.vp3d_recipe2()
+                    self.new_pred=True
+                else:
+                    self.new_pred=False
+
+                pic_key = picture_keypoints(personwiseKeypoints,keypoints_list,image,after_shape)
+
+                _, jpeg = cv2.imencode('.jpg', pic_key)
+                return  jpeg.tobytes()
+
+    def get_prediction(self):
+        return json.dumps({'action':self.prediction,'rating':self.rating})
+        
+    def vp3d_recipe2(self):
+        
+        clip_df = interpolate(self.clip_df,interpolate_feet=False)
+        
+        clip_df =  delete_nans(clip_df)
+        multiplier = round(800/224,2)
+        clip_df =  rescale_keypoints(clip_df,multiplier)
+
+        actions, poses = fetch_keypoints(clip_df)
+        classes = 8
+
+        pretrained_weights = torch.load(chk_filename, map_location=lambda storage, loc: storage)
+        model = NaiveBaselineModel(in_joints, in_dims, out_joints, filter_widths, pretrained_weights, embedding_len, classes,
+                                    causal=True, dropout=0.25, channels=channels)
+        receptive_field = eval_model.base_model.receptive_field()
+        pad = (receptive_field - 1) 
+        checkp = torch.load('/home/artursil/Documents/virtual_trainer/Virtual_trainer/checkpoint/Recipe-2-epoch-19.pth')
+        # checkp = torch.load('/home/artursil/Documents/virtual_trainer/Virtual_trainer/checkpoint/model-6.pth')
+        checkp['model_state_dict']
+        model.load_state_dict(checkp['model_state_dict'])
+
+        with torch.no_grad():
+            model.eval() 
+            if torch.cuda.is_available():
+                model = model.cuda()
+                # poses = poses.cuda()
+            try:
+                poses = np.concatenate(poses)
+            except ValueError:
+                self.prediction = "No human detected"
+                return self
+            poses = np.pad(poses,((54,0),(0,0),(0,0)),'edge')
+            poses = torch.Tensor(np.expand_dims(poses,axis=0)).cuda()
+            pred = eval_model(poses)
+            softmax = torch.nn.Softmax(1)
+            pred= softmax(pred)
+            pred = pred.detach().cpu().numpy().squeeze()
+            print(pred)
+            preds = np.argmax(pred,axis=0)
+            print(preds)
+            values, counts = np.unique(preds,return_counts=True)
+            print(values)
+            print(counts)
+            ind = np.argmax(counts)
+            self.prediction = EXC_DICT[values[ind]]
+            self.rating=9
+            return self
         
     # def vp3d_model(self):
         
