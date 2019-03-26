c502ef162e36aa9e5c8781f9c50fcd77c1b6e72b
Ahmad <ahmadofski@gmail.com>
2019-03-26 14:08:48 +0100

    front end

--- webcam/main.py
+++ webcam/main.py
@@ -7,7 +7,6 @@ from openpose.model import get_model
 from flask import Flask, render_template, Response
 from camera import VideoCamera
 from webcam_model import ModelClass
-import numpy as np
 
 app = Flask(__name__)
 
@@ -43,7 +42,7 @@ model = torch.nn.DataParallel(model)
 model = model.cuda()
 model.float()
 
-camera_index = 1
+camera_index = 0
 predict_cl = ModelClass(model,camera_index)
 
 @app.route('/')
@@ -54,24 +53,19 @@ def index():
 def openpose():
     return render_template('openpose.html')
 
+@app.route("/stream")
+def stream():
+    def eventStream():
+        while True:
+            if predict_cl.new_pred:
+                yield "data: {}\n\n".format(predict_cl.get_prediction())
+    
+    return Response(eventStream(), mimetype="text/event-stream")
+
 @app.route('/vp3d')
 def vp3d():
-    array,n_frames = predict_cl.vp3d_model()
-    return render_template('vp3d.html',prediction = predict_cl.prediction, array=array,n_frames=n_frames)
-
-@app.route('/vp3d2')
-def vp3d2():
-    array,n_frames = predict_cl.vp3d_model()
-    array = array.tolist()
-#     array = np.load('/media/artursil/backup/class_1.npy')
-#     array *= np.array([1,-1,1])
-#     print(array)
-#     print(array.shape)
-#     n_frames = array.shape[0]
-#     array = array.flatten().tolist()
-#     print(array)
-    return render_template('rend_skel.html',prediction = predict_cl.prediction, array_3d=array,n_frames=n_frames)
-#     return render_template('rend_skel.html',prediction = 0, array_3d=array,n_frames=n_frames)
+    predict_cl.vp3d_model()
+    return render_template('vp3d.html',prediction = predict_cl.prediction)
 
 def gen(camera):
     while True:
@@ -81,7 +75,7 @@ def gen(camera):
 
 def gen_model(predict_cl):
     while True:
-        frame = predict_cl.get_keypoints()
+        frame = predict_cl.get_keypoints2()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -98,13 +92,6 @@ def openpose_feed():
     return Response(gen_model(predict_cl),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/kp_array')
-def get_nparray():
-    # serialises the 3d keypoints
-    kp3d = predict_cl.kp_buff.astype('float32')
-    response = flask.make_response(kp3d.tobytes())
-    response.headers.set('Content-Type', 'application/octet-stream')
-    return response
 
 if __name__ == '__main__':
     app.run(host='localhost', threaded=True)
--- webcam/static/openpose.js
+++ webcam/static/openpose.js
@@ -1,5 +1,16 @@
+var evtSource = new EventSource("/stream");
+
+evtSource.onmessage = function(e) {
+  var classify = document.getElementById('classify');
+  var rating = document.getElementById('rating');
+  var obj = JSON.parse(e.data);
+
+  classify.innerHTML = "Exercise: " + e.action;
+  rating.innerHTML = "Exercise: " + e.rating;
+}
+
 window.onload = function(){ 
   document.getElementById("button1").onclick = function () {
-      location.href = "/vp3d2";
+      location.href = "/";
   };
 };
